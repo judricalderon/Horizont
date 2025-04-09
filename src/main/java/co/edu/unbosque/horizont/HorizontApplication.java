7ae312ecdeeb4ce723cb352d4c031c1412d7766a
judricalderon <judricalderon@gmail.com>
2025-04-08 23:15:36 -0500

    mi primer commit, inicio del proyecto

---
+++ src/main/java/co/edu/unbosque/horizont/HorizontApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.horizont;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorizontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorizontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/unbosque/horizont/HorizontApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.horizont;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorizontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
