aae92c24b22a8a1c61296039088826a5ac1a11f0
Alexander-canon <jcanonp@unbosque.edu.co>
2025-04-24 01:06:37 -0500

    prueba2

--- src/main/java/co/edu/unbosque/horizont/config/ModelMapperConfig.java
+++ src/main/java/co/edu/unbosque/horizont/config/ModelMapperConfig.java
@@ -1,8 +1,8 @@
 package co.edu.unbosque.horizont.config;
 
-import org.modelmapper.ModelMapper;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.modelmapper.ModelMapper; // import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean; // import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration; // import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ModelMapperConfig {
