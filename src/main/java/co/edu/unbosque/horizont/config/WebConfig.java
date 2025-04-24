8a73bbfcefeb86ddb08ff062378b4028d2a64100
judricalderon <judricalderon@gmail.com>
2025-04-24 00:19:16 -0500

    se añade configuraciones

--- src/main/java/co/edu/unbosque/horizont/config/WebConfig.java
+++ src/main/java/co/edu/unbosque/horizont/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // o donde tengas tu frontend
+                .allowedOrigins("http://localhost:5501", "http://127.0.0.1:5504") // agrega ambos
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
